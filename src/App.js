d86f3a5bf042a5cf833224b865b2878442ac64dd
Ewelina <ewelina.petelicka01@gmail.com>
2023-05-24 19:49:10 +0200

    calculator layout

---
+++ src/App.js
@@ -0,0 +1,35 @@
+export function App() {
+    return (
+        <div className={'page'}>
+            <div className={'container'}>
+                <div className={'screen'}>
+
+                </div>
+                <div className={'row'}>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>*</button>
+                </div>
+                <div className={'row'}>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>/</button>
+                </div>
+                <div className={'row'}>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>+</button>
+                </div>
+                <div className={'row'}>
+                    <button>C</button>
+                    <button>0</button>
+                    <button>,</button>
+                    <button>-</button>
+                </div>
+            </div>
+        </div>
+    );
+}
