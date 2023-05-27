9c4df0a6735b6646d4b21efdb8c713af30bc3392
Ewelina <ewelina.petelicka01@gmail.com>
2023-05-27 10:49:36 +0200

    calc method improvements

--- src/App.js
+++ src/App.js
@@ -29,25 +29,50 @@ export function App() {
                     <button onClick={()=>setValue(value + "4")}>4</button>
                     <button onClick={()=>setValue(value + "5")}>5</button>
                     <button onClick={()=>setValue(value + "6")}>6</button>
-                    <button>÷</button>
+                    <button onClick={()=>{
+                        setValue('');
+                        setResult(value);
+                        setSymbol(' ÷ ');
+                    }}>÷</button>
                 </div>
                 <div className={'row'}>
                     <button onClick={()=>setValue(value + "7")}>7</button>
                     <button onClick={()=>setValue(value + "8")}>8</button>
                     <button onClick={()=>setValue(value + "9")}>9</button>
-                    <button>+</button>
+                    <button onClick={()=>{
+                        setValue('');
+                        setResult(value);
+                        setSymbol(' + ');
+                    }}>+</button>
                 </div>
                 <div className={'row'}>
-                    <button>C</button>
+                    <button onClick={()=>{
+                        setValue('');
+                        setResult('');
+                        setSymbol('');
+                    }}>C</button>
                     <button onClick={()=>setValue(value + "0")}>0</button>
                     <button onClick={()=>setValue(value + ",")}>,</button>
-                    <button>-</button>
+                    <button onClick={()=>{
+                        setValue('');
+                        setResult(value);
+                        setSymbol(' - ');
+                    }}>-</button>
                 </div>
                 <button onClick={()=> {
                     const a = Number(result.replace(',','.'));
                     const b = Number(value.replace(',','.'));
                     if (symbol === ' × ') {
                         setValue((a * b).toString().replace('.',','));
+                    }
+                    if (symbol === ' ÷ ' ) {
+                        setValue((a / b).toString().replace('.',','))
+                    }
+                    if (symbol === ' + ' ) {
+                        setValue((a + b).toString().replace('.',','))
+                    }
+                    if (symbol === ' - ' ) {
+                        setValue((a - b).toString().replace('.',','))
                     }
                     setSymbol('');
                     setResult('');
